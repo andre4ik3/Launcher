005bf1efb6fe1067613a5bfe227533005ca28788
andre4ik3 <andre4ik3@fastmail.com>
2025-01-22 12:59:23 +0400

    Nixification

--- apps/cli/src/main.rs
+++ apps/cli/src/main.rs
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use core::*;
+use launcher::{utils, net, fetch};
 use url::Url;
 
 const BASE_URL: &str = "https://launchermeta.lambda.prod.andre4ik3.net/";
--- flake.nix
+++ flake.nix
@@ -6,32 +6,128 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { nixpkgs, flake-utils, fenix, ... }: flake-utils.lib.eachDefaultSystem (system: let
+  nixConfig = {
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+  };
+
+  outputs = { nixpkgs, flake-utils, fenix, crane, ... }: flake-utils.lib.eachDefaultSystem (system: let
     pkgs = import nixpkgs { inherit system; overlays = [ fenix.overlays.default ]; };
-    lib = nixpkgs.lib;
-  in {
-    devShells.default = pkgs.mkShell {
-      packages = with pkgs; [
-        # Core modules (Rust)
-        (pkgs.fenix.complete.withComponents [
-          "cargo"
-          "clippy"
-          "rust-src"
-          "rustc"
-          "rustfmt"
-          "rust-analyzer"
-        ])
-        cargo-deny
-        llvmPackages.clangWithLibcAndBasicRtAndLibcxx
-      ] ++ (lib.optionals pkgs.hostPlatform.isLinux [
-        pkg-config
-        openssl
+    inherit (pkgs) lib;
+
+    toolchain = (pkgs.fenix.complete.withComponents [
+      "cargo"
+      "clippy"
+      "llvm-tools"
+      "rust-src"
+      "rustc"
+      "rustfmt"
+      "rust-analyzer"
+    ]);
+
+    craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+    src = craneLib.cleanCargoSource ./.;
+
+    commonArgs = {
+      inherit src;
+      strictDeps = true;
+      nativeBuildInputs = with pkgs; [ pkg-config ];
+      buildInputs = with pkgs; [
+        # Add additional build inputs here
+        #llvmPackages.clangWithLibcAndBasicRtAndLibcxx
+        glib
+        dbus
         gtk4
         libadwaita
+      ] ++ lib.optionals stdenv.isDarwin [
+        # Additional darwin specific inputs can be set here
+#        pkgs.libiconv
+      ];
+
+      # Additional environment variables can be set directly
+      # MY_CUSTOM_VAR = "some value";
+    };
+
+    # Build *just* the cargo dependencies (of the entire workspace),
+    # so we can reuse all of that work (e.g. via cachix) when running in CI
+    # It is *highly* recommended to use something like cargo-hakari to avoid
+    # cache misses when building individual top-level-crates
+    cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+    individualCrateArgs = commonArgs // {
+      inherit cargoArtifacts;
+      inherit (craneLib.crateNameFromCargoToml { inherit src; }) version;
+    };
+
+    fileSetForCrate = { crate, extras ? [] }: lib.fileset.toSource {
+      root = ./.;
+      fileset = lib.fileset.unions ([
+        ./Cargo.toml
+        ./Cargo.lock
+        (craneLib.fileset.commonCargoSources ./modules)
+        (craneLib.fileset.commonCargoSources ./core)
+        (craneLib.fileset.commonCargoSources crate)
+      ] ++ extras);
+    };
+  in rec {
+    packages = {
+      default = packages.launcher-cli;
+      launcher-cli = craneLib.buildPackage (individualCrateArgs // {
+        pname = "launcher-cli";
+        cargoExtraArgs = "-p mc";
+        src = fileSetForCrate { crate = ./apps/cli; };
+      });
+      launcher-linux = craneLib.buildPackage (individualCrateArgs // {
+        pname = "launcher-linux";
+        cargoExtraArgs = "-p launcher-linux";
+        src = fileSetForCrate { crate = ./apps/linux; extras = [ ./apps/linux/resources ]; };
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          glib
+          dbus
+          gtk4
+          libadwaita
+        ];
+        buildInputs = with pkgs; [
+          glib
+          dbus
+          gtk4
+          libadwaita
+        ];
+      });
+      silo = craneLib.buildPackage (individualCrateArgs // {
+        pname = "silo";
+        cargoExtraArgs = "-p silo";
+        src = fileSetForCrate { crate = ./silo; };
+      });
+    };
+
+    apps = rec {
+      default = cli;
+      cli = flake-utils.lib.mkApp {
+        name = "mc";
+        drv = packages.launcher-cli;
+      };
+      linux = flake-utils.lib.mkApp {
+        drv = packages.launcher-linux;
+      };
+    };
+
+    devShells.default = craneLib.devShell {
+      packages = with pkgs; [
+        # Core modules (Rust)
+        cargo-deny
+        llvmPackages.clangWithLibcAndBasicRtAndLibcxx
+      ] ++ (lib.optionals hostPlatform.isLinux [
+        pkg-config
+        glib
         dbus
-      ]) ++ (lib.optionals pkgs.hostPlatform.isDarwin [
+        gtk4
+        libadwaita
+      ]) ++ (lib.optionals hostPlatform.isDarwin [
         swift-format
         xcodegen
       ]);
